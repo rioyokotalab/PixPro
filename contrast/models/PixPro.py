f15be30f74714f7edae539b1a2b5707d300c9326
tomo <tomo14142356@gmail.com>
2022-06-16 12:26:46 +0900

    Add flowe loss using grid sample

--- contrast/models/PixPro.py
+++ contrast/models/PixPro.py
@@ -40,6 +40,7 @@ class MLP2d(nn.Module):
 
         return x
 
+
 def regression_loss_same(q, k):
     """ q, k: N * C * H * W
         coord_q, coord_k: N * 4 (x_upper_left, y_upper_left, x_lower_right, y_lower_right)
@@ -54,7 +55,7 @@ def regression_loss_same(q, k):
     k = k.view(N, C, -1)
 
     diag_mask = torch.diag(torch.ones(H * W, dtype=torch.bool)).to(device)
-    pos_mask = diag_mask
+    pos_mask = diag_mask.float()
     # if torch.distributed.get_rank() == 0:
     #     print("pos_mask_sum_shape:", pos_mask.sum(-1).sum(-1).shape, pos_mask.sum(-1).shape, pos_mask.shape)
     #     print("pos_mask_sum:", pos_mask.sum(-1).sum(-1), pos_mask.sum(-1))
@@ -63,6 +64,68 @@ def regression_loss_same(q, k):
     logit = torch.bmm(q.transpose(1, 2), k)
 
     loss = (logit * pos_mask).sum(-1).sum(-1) / (pos_mask.sum(-1).sum(-1) + 1e-6)
+
+    return -2 * loss.mean()
+
+
+def flowe_loss(q, k, coord_q, coord_k):
+    """ q, k: N * C * H * W
+        coord_q, coord_k: N * 4 (x_upper_left, y_upper_left, x_lower_right, y_lower_right)
+    """
+    N, C, H, W = q.shape
+    # [bs, feat_dim, 49]
+    q = q.view(N, C, -1)
+    k = k.view(N, C, -1)
+
+    # generate center_coord, width, height
+    # [1, 7, 7]
+    x_array = torch.arange(0., float(W), dtype=coord_q.dtype, device=coord_q.device).view(1, 1, -1).repeat(1, H, 1)
+    y_array = torch.arange(0., float(H), dtype=coord_q.dtype, device=coord_q.device).view(1, -1, 1).repeat(1, 1, W)
+    # [bs, 1, 1]
+    q_bin_width = ((coord_q[:, 2] - coord_q[:, 0]) / W).view(-1, 1, 1)
+    q_bin_height = ((coord_q[:, 3] - coord_q[:, 1]) / H).view(-1, 1, 1)
+    # k_bin_width = ((coord_k[:, 2] - coord_k[:, 0]) / W).view(-1, 1, 1)
+    # k_bin_height = ((coord_k[:, 3] - coord_k[:, 1]) / H).view(-1, 1, 1)
+    k_bin_width = ((coord_k[:, 2] - coord_k[:, 0])).view(-1, 1, 1)
+    k_bin_height = ((coord_k[:, 3] - coord_k[:, 1])).view(-1, 1, 1)
+    # [bs, 1, 1]
+    q_start_x = coord_q[:, 0].view(-1, 1, 1)
+    q_start_y = coord_q[:, 1].view(-1, 1, 1)
+    k_start_x = coord_k[:, 0].view(-1, 1, 1)
+    k_start_y = coord_k[:, 1].view(-1, 1, 1)
+
+    # [bs, 7, 7]
+    center_q_x = (x_array + 0.5) * q_bin_width + q_start_x
+    center_q_y = (y_array + 0.5) * q_bin_height + q_start_y
+    # if torch.distributed.get_rank() == 0:
+    #     print("qx", center_q_x[:, 0, -1], coord_q[:, 2])
+    #     print("qy", center_q_y[:, -1, 0], coord_q[:, 3])
+    #     print("ky", center_k_y[:, -1, 0], coord_k[:, 3])
+    #     print("kx", center_k_x[:, 0, -1], coord_k[:, 2])
+
+    # [bs, 7, 7]
+    relative_center_q_x = (center_q_x - k_start_x) / k_bin_width
+    relative_center_q_y = (center_q_y - k_start_y) / k_bin_height
+    relative_center_q_x_norm = relative_center_q_x * 2 - 1
+    relative_center_q_y_norm = relative_center_q_y * 2 - 1
+
+    pos_mask_x = torch.abs(relative_center_q_x_norm) < 1
+    pos_mask_y = torch.abs(relative_center_q_y_norm) < 1
+    pos_mask = pos_mask_x & pos_mask_y
+
+    # [bs, 2, 7, 7]
+    grid_k = torch.stack(relative_center_q_x_norm, relative_center_q_y_norm)
+    # [bs, 7, 7, 2]
+    grid_k = grid_k.permute(0, 2, 3, 1)
+
+    q_mask = q.permute(0, 2, 3, 1)[pos_mask]
+    k_mask = F.grid_sample(k, grid_k, align_corners=True)
+    k_mask = k_mask.permute(0, 2, 3, 1)[pos_mask]
+    if torch.distributed.get_rank() == 0:
+        print("q_mask", q_mask.shape)
+        print("k_mask", k_mask.shape)
+
+    loss = F.cosine_similarity(q_mask, k_mask, -1, 1e-6)
 
     return -2 * loss.mean()
 
@@ -79,8 +142,9 @@ def regression_loss(q, k, coord_q, coord_k, pos_ratio=0.5, is_flowe=False, same_
     q = q.view(N, C, -1)
     k = k.view(N, C, -1)
     if is_flowe:
-        max_norm_diag = (1 / H) ** 2 + (1 / W) ** 2
-        pos_ratio = torch.sqrt(torch.tensor(max_norm_diag)) / 2
+        # max_norm_diag = (1 / H) ** 2 + (1 / W) ** 2
+        # pos_ratio = torch.sqrt(torch.tensor(max_norm_diag)) / 2
+        return flowe_loss(q, k, coord_q, coord_k)
 
     # generate center_coord, width, height
     # [1, 7, 7]
