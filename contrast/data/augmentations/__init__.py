f07e9097f8f5e40ff3d9e9070f261765cd728ccd
tomo <tomo14142356@gmail.com>
2022-07-11 12:30:01 +0900

    Fix bug for import sentence

--- contrast/data/augmentations/__init__.py
+++ contrast/data/augmentations/__init__.py
@@ -1,4 +1,6 @@
 # from . import sequence_augmentations, segmentation_augmentations, flow_augmentations
-from . import segmentation_augmentations, flow_augmentations
-from . import preprocess_augmentations
-from .color_augmentations import RandomApply, GaussianBlur
+# from . import segmentation_augmentations, flow_augmentations
+# from . import preprocess_augmentations
+from . import flow_augmentations
+from . import color_augmentations
+# from .color_augmentations import RandomApply, GaussianBlur
