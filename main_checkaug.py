39cdee8c974cedec4b44d231efda228cdecd6536
tomo <tomo14142356@gmail.com>
2022-07-31 15:50:45 +0900

    Organize check img script

--- main_checkaug.py
+++ main_checkaug.py
@@ -6,7 +6,6 @@ from shutil import copyfile
 import torch
 import torch.distributed as dist
 from torch.backends import cudnn
-from torch.nn.parallel import DistributedDataParallel
 from torch.utils.data.distributed import DistributedSampler
 from torch.utils.tensorboard import SummaryWriter
 import torchvision
@@ -14,14 +13,14 @@ import torchvision
 import wandb
 from contrast.logger import init_wandb, get_save_files
 
-from contrast import models
-from contrast import resnet
 from contrast.data import get_loader
 from contrast.logger import setup_logger
 from contrast.lr_scheduler import get_scheduler
 from contrast.option import parse_option
 from contrast.util import AverageMeter
-from contrast.lars import add_weight_decay, LARS
+
+from main_pretrain import build_model
+from main_pretrain import load_pretrained, load_checkpoint
 
 try:
     # noinspection PyUnresolvedReferences
@@ -30,76 +29,25 @@ except ImportError:
     amp = None
 
 
-def build_model(args):
-    encoder = resnet.__dict__[args.arch]
-    model = models.__dict__[args.model](encoder, args).cuda()
-
-    if args.optimizer == 'sgd':
-        optimizer = torch.optim.SGD(
-            model.parameters(),
-            lr=args.batch_size * dist.get_world_size() / 256 * args.base_learning_rate,
-            momentum=args.momentum,
-            weight_decay=args.weight_decay,)
-    elif args.optimizer == 'lars':
-        params = add_weight_decay(model, args.weight_decay)
-        optimizer = torch.optim.SGD(
-            params,
-            lr=args.batch_size * dist.get_world_size() / 256 * args.base_learning_rate,
-            momentum=args.momentum,)
-        optimizer = LARS(optimizer)
-    else:
-        raise NotImplementedError
-
-    if args.amp_opt_level != "O0":
-        model, optimizer = amp.initialize(model, optimizer, opt_level=args.amp_opt_level)
-
-    model = DistributedDataParallel(model, device_ids=[args.local_rank], broadcast_buffers=False)
-
-    return model, optimizer
-
-
-def load_pretrained(model, pretrained_model):
-    ckpt = torch.load(pretrained_model, map_location='cpu')
-    state_dict = ckpt['model']
-    model_dict = model.state_dict()
-
-    model_dict.update(state_dict)
-    model.load_state_dict(model_dict)
-    logger.info(f"==> loaded checkpoint '{pretrained_model}' (epoch {ckpt['epoch']})")
-
-
-def load_checkpoint(args, model, optimizer, scheduler, sampler=None):
-    logger.info(f"=> loading checkpoint '{args.resume}'")
-
-    checkpoint = torch.load(args.resume, map_location='cpu')
-    args.start_epoch = checkpoint['epoch'] + 1
-    model.load_state_dict(checkpoint['model'])
-    optimizer.load_state_dict(checkpoint['optimizer'])
-    scheduler.load_state_dict(checkpoint['scheduler'])
-
-    if args.amp_opt_level != "O0" and checkpoint['opt'].amp_opt_level != "O0":
-        amp.load_state_dict(checkpoint['amp'])
-
-    logger.info(f"=> loaded successfully '{args.resume}' (epoch {checkpoint['epoch']})")
-
-    del checkpoint
-    torch.cuda.empty_cache()
-
-
-def save_checkpoint(args, epoch, model, optimizer, scheduler, sampler=None):
-    logger.info('==> Saving...')
-    state = {
-        'opt': args,
-        'model': model.state_dict(),
-        'optimizer': optimizer.state_dict(),
-        'scheduler': scheduler.state_dict(),
-        'epoch': epoch,
-    }
-    if args.amp_opt_level != "O0":
-        state['amp'] = amp.state_dict()
-    file_name = os.path.join(args.output_dir, f'ckpt_epoch_{epoch}.pth')
-    torch.save(state, file_name)
-    copyfile(file_name, os.path.join(args.output_dir, 'current.pth'))
+def dist_setup():
+    master_addr = os.getenv("MASTER_ADDR", default="localhost")
+    master_port = os.getenv("MASTER_PORT", default="8888")
+    method = "tcp://{}:{}".format(master_addr, master_port)
+    rank = int(os.getenv("OMPI_COMM_WORLD_RANK", "0"))
+    world_size = int(os.getenv("OMPI_COMM_WORLD_SIZE", "1"))
+    local_rank = int(os.getenv("OMPI_COMM_WORLD_LOCAL_RANK", "-1"))
+    local_size = int(os.getenv("OMPI_COMM_WORLD_LOCAL_SIZE", "-2"))
+    node_rank = int(os.getenv("OMPI_COMM_WORLD_NODE_RANK", "-3"))
+    host_port_str = f"host: {master_addr}, port: {master_port}"
+    print(
+        "rank: {}, world_size: {}, local_rank: {}, local_size: {}, node_rank: {}, {}"
+        .format(rank, world_size, local_rank, local_size, node_rank, host_port_str))
+    dist.init_process_group("nccl", init_method=method, rank=rank,
+                            world_size=world_size)
+    print("Rank: {}, Size: {}, Host: {} Port: {}".format(dist.get_rank(),
+                                                         dist.get_world_size(),
+                                                         master_addr, master_port))
+    return local_rank
 
 
 def main(args):
@@ -109,6 +57,8 @@ def main(args):
         two_crop=args.model in ['PixPro'],
         prefix=train_prefix,
         return_coord=True,)
+    # train_loader.dataset.cuda()
+    # train_loader.data.cuda()
 
     args.num_instances = len(train_loader.dataset)
     logger.info(f"length of training dataset: {args.num_instances}")
@@ -142,13 +92,10 @@ def main(args):
             train_loader.sampler.set_epoch(epoch)
 
         save_img(epoch, train_loader, model, optimizer, scheduler, args, summary_writer)
-        # train(epoch, train_loader, model, optimizer, scheduler, args, summary_writer)
-
-        # if dist.get_rank() == 0 and (epoch % args.save_freq == 0 or epoch == args.epochs):
-        #     save_checkpoint(args, epoch, model, optimizer, scheduler, sampler=train_loader.sampler)
 
         if epoch >= args.debug_epochs:
             break
+
 
 def save_img(epoch, train_loader, model, optimizer, scheduler, args, summary_writer):
     """
@@ -177,9 +124,9 @@ def save_img(epoch, train_loader, model, optimizer, scheduler, args, summary_wri
         save_imgs = []
         if im1.ndim > 3:
             print("in cod")
-            im1 = im1.reshape(bn, -1, c, h, w) 
-            im2 = im2.reshape(bn, -1, c, h, w) 
-            num_img = im1.shape[1] 
+            im1 = im1.reshape(bn, -1, c, h, w)
+            im2 = im2.reshape(bn, -1, c, h, w)
+            num_img = im1.shape[1]
             for i in range(num_img):
                 for j in range(bn):
                     save_imgs.append(torch.stack([im1[j][i], im2[j][i]]))
@@ -199,77 +146,19 @@ def save_img(epoch, train_loader, model, optimizer, scheduler, args, summary_wri
             f'Train: [{epoch}/{args.epochs}][{idx}/{train_len}]  '
             f'Time {batch_time.val:.3f} ({batch_time.avg:.3f})  ')
 
-def train(epoch, train_loader, model, optimizer, scheduler, args, summary_writer):
-    """
-    one epoch training
-    """
-    model.train()
 
-    batch_time = AverageMeter()
-    loss_meter = AverageMeter()
-
-    end = time.time()
-    for idx, data in enumerate(train_loader):
-        data = [item.cuda(non_blocking=True) for item in data]
-
-        # In PixPro, data[0] -> im1, data[1] -> im2, data[2] -> coord1, data[3] -> coord2
-        loss = model(data[0], data[1], data[2], data[3])
-
-        # backward
-        optimizer.zero_grad()
-        if args.amp_opt_level != "O0":
-            with amp.scale_loss(loss, optimizer) as scaled_loss:
-                scaled_loss.backward()
-        else:
-            loss.backward()
-        optimizer.step()
-        scheduler.step()
-
-        # update meters and print info
-        loss_meter.update(loss.item(), data[0].size(0))
-        batch_time.update(time.time() - end)
-        end = time.time()
-
-        train_len = len(train_loader)
-        if idx % args.print_freq == 0:
-            lr = optimizer.param_groups[0]['lr']
-            logger.info(
-                f'Train: [{epoch}/{args.epochs}][{idx}/{train_len}]  '
-                f'Time {batch_time.val:.3f} ({batch_time.avg:.3f})  '
-                f'lr {lr:.3f}  '
-                f'loss {loss_meter.val:.3f} ({loss_meter.avg:.3f})')
-
-            # tensorboard logger
-            if summary_writer is not None:
-                step = (epoch - 1) * len(train_loader) + idx
-                summary_writer.add_scalar('lr', lr, step)
-                summary_writer.add_scalar('loss', loss_meter.val, step)
-
-        if dist.get_rank() == 0:
-            global_step = (epoch - 1) * train_len + idx
-            wandb.log({"lr": lr, "loss": loss_meter.val, "loss/avg": loss_meter.avg,
-                "epoch": epoch - 1, "global_step": global_step, "time": batch_time.val,
-                "time/avg": batch_time.avg})
-
-if __name__ == '__main__':
-    opt = parse_option(stage='pre-train')
-
-    if opt.amp_opt_level != "O0":
-        assert amp is not None, "amp not installed!"
-
-    torch.cuda.set_device(opt.local_rank)
-    torch.distributed.init_process_group(backend='nccl', init_method='env://')
-    cudnn.benchmark = True
-
+def main_prog(opt):
     # setup logger
     os.makedirs(opt.output_dir, exist_ok=True)
-    logger = setup_logger(output=opt.output_dir, distributed_rank=dist.get_rank(), name="contrast")
+    global logger
+    logger = setup_logger(output=opt.output_dir,
+                          distributed_rank=dist.get_rank(), name="contrast")
     if dist.get_rank() == 0:
         path = os.path.join(opt.output_dir, "config.json")
         with open(path, 'w') as f:
             json.dump(vars(opt), f, indent=2)
         logger.info("Full config saved to {}".format(path))
-        init_wandb(opt, "ssl_test_result")
+        init_wandb(opt)
         wandb.save(path, base_path=opt.output_dir)
 
     # print args
@@ -305,3 +194,22 @@ if __name__ == '__main__':
                     copyfile(f, new_f)
             wandb.save(new_f, base_path=out_root)
 
+
+if __name__ == '__main__':
+    opt = parse_option(stage='pre-train')
+
+    if opt.amp_opt_level != "O0":
+        assert amp is not None, "amp not installed!"
+
+    # mpirun
+    local_rank = dist_setup()
+    opt.local_rank = local_rank
+
+    torch.cuda.set_device(opt.local_rank)
+
+    # # pytorch distrubuted run
+    # torch.distributed.init_process_group(backend='nccl', init_method='env://')
+
+    cudnn.benchmark = True
+
+    main_prog(opt)
