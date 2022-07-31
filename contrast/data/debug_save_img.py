e0beedf74f44681e103a32aa9de9c871a3627856
tomo <tomo14142356@gmail.com>
2022-07-31 15:33:13 +0900

    Add coord grid sample debug img

--- contrast/data/debug_save_img.py
+++ contrast/data/debug_save_img.py
@@ -1,11 +1,15 @@
+from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 
+import torch
 from torchvision.transforms import functional as F
 import torch.nn.functional as nnF
 
 
-def debug_img_save(img_tensor, orig_img_pil, out_coord, out_grid, idx=1, calc_coord=None, is_corner=True):
+def debug_img_save(img_tensor, orig_img_pil, out_coord, out_grid, idx=1,
+                   calc_coord=None, is_corner=True):
+    frame_name = f"{idx} frame"
     orig_img = F.to_tensor(orig_img_pil).clone()
     grid = out_grid.clone()
     coord = out_coord.clone()
@@ -20,7 +24,7 @@ def debug_img_save(img_tensor, orig_img_pil, out_coord, out_grid, idx=1, calc_co
     to_pad_resize = Resize_with_pad(w, h)
 
     img = to_pad_resize(img)
-    img_str = add_str_img(f"official data aug img on {idx} frame", img)
+    img_str = add_str_img(f"official data aug img on {frame_name}", img)
 
     orig_img = orig_img.unsqueeze(0)
 
@@ -29,7 +33,8 @@ def debug_img_save(img_tensor, orig_img_pil, out_coord, out_grid, idx=1, calc_co
     # resize_grid = resize_grid.unsqueeze(0).permute(0, 2, 3, 1)
     # img_tmp_resize = nnF.grid_sample(orig_img, resize_grid, align_corners=is_corner)
     # img_tmp_resize = to_pad_resize(img_tmp_resize[0])
-    # img_tmp_resize = add_str_img(f"down size my resized data aug img on {idx} frame by pad", img_tmp_resize)
+    # str_down = f"down size my resized data aug img on {idx} frame by pad"
+    # img_tmp_resize = add_str_img(str_down, img_tmp_resize)
 
     grid_tmp = grid.unsqueeze(0).permute(0, 2, 3, 1)
     coord_tmp = coord.unsqueeze(0).permute(0, 2, 3, 1)
@@ -38,33 +43,50 @@ def debug_img_save(img_tensor, orig_img_pil, out_coord, out_grid, idx=1, calc_co
         calc_grid_tmp = calc_grid.unsqueeze(0).permute(0, 2, 3, 1)
         img_calc_tmp = nnF.grid_sample(orig_img, calc_grid_tmp, align_corners=is_corner)
         img_calc_tmp = to_pad_resize(img_calc_tmp)
-        img_calc_tmp = add_str_img(f"data aug by using official grid img on {idx} frame", img_calc_tmp[0])
-        inv_img_calc_tmp = nnF.grid_sample(img.unsqueeze(0), calc_grid_tmp, align_corners=is_corner)
+        add_str = f"data aug by using official grid img on {frame_name}"
+        img_calc_tmp = add_str_img(add_str, img_calc_tmp[0])
+        inv_img_calc_tmp = nnF.grid_sample(img.unsqueeze(0), calc_grid_tmp,
+                                           align_corners=is_corner)
         inv_img_calc_tmp = to_pad_resize(inv_img_calc_tmp)
-        inv_img_calc_tmp = add_str_img(f"inv data aug by using official grid img from officical data aug on {idx} frame", inv_img_calc_tmp[0])
+        add_str = "inv data aug by using official grid img from officical data aug on"
+        add_str += f" {frame_name}"
+        inv_img_calc_tmp = add_str_img(add_str, inv_img_calc_tmp[0])
 
     down_size_orig_img = to_pad_resize(orig_img[0])
     # down_size_orig_img = to_pad_resize(orig_img)
-    down_size_orig_img = add_str_img(f"down size orginal img on {idx} frame", down_size_orig_img)
+    add_str = f"down size orginal img on {frame_name}"
+    down_size_orig_img = add_str_img(add_str, down_size_orig_img)
 
     down_size_img_tmp = to_pad_resize(img_tmp[0])
     # down_size_img_tmp = to_pad_resize(img_tmp)
     # down_size_img_tmp = down_size_img_tmp[0]
-    down_size_img_tmp = add_str_img(f"down size my data aug img on {idx} frame by pad", down_size_img_tmp)
+    add_str = f"down size my data aug img on {frame_name} by pad"
+    down_size_img_tmp = add_str_img(add_str, down_size_img_tmp)
 
     img_tmp_crop = F.resize(img_tmp[0], [h, w])
     # img_tmp_crop = nnF.interpolate(img_tmp, (h, w),
     #                                mode='bilinear',
     #                                align_corners=is_corner)
     # img_tmp_crop = img_tmp_crop[0]
-    img_tmp_crop = add_str_img(f"down size my data aug img on {idx} frame by resize", img_tmp_crop)
+    add_str = f"down size my data aug img on {frame_name} by resize"
+    img_tmp_crop = add_str_img(add_str, img_tmp_crop)
 
-    # inv_img = nnF.grid_sample(img.unsqueeze(0), coord_tmp, align_corners=is_corner)
-    # inv_img = add_str_img(f"inv my data aug img from offical data aug img on {idx} frame", inv_img[0])
-    # inv_img_tmp = nnF.grid_sample(img_tmp, coord_tmp, align_corners=is_corner)
-    # inv_img_tmp = add_str_img(f"inv my data aug img from my data aug img which no resize on {idx} frame", inv_img_tmp[0])
-    # inv_img_tmp_crop = nnF.grid_sample(img_tmp_crop.unsqueeze(0), coord_tmp, align_corners=is_corner)
-    # inv_img_tmp_crop = add_str_img(f"inv my data aug img from my data aug img which has resize on {idx} frame", inv_img_tmp_crop[0])
+    inv_img = nnF.grid_sample(img.unsqueeze(0), coord_tmp, align_corners=is_corner)
+    add_str = f"inv my data aug img from offical data aug img on {frame_name}"
+    inv_img = add_str_img(add_str, inv_img[0])
+    inv_img_tmp = nnF.grid_sample(img_tmp, coord_tmp, align_corners=is_corner)
+    add_str = "inv my data aug img from my data aug img which no resize on "
+    add_str += f"{frame_name}"
+    inv_img_tmp = add_str_img(add_str, inv_img_tmp[0])
+    inv_orig_img_tmp = nnF.grid_sample(orig_img, coord_tmp, align_corners=is_corner)
+    add_str = "inv orig img my data aug img from my data aug img which no resize on "
+    add_str += f"{frame_name}"
+    inv_orig_img_tmp = add_str_img(add_str, inv_orig_img_tmp[0])
+    # inv_img_tmp_crop = nnF.grid_sample(img_tmp_crop.unsqueeze(0), coord_tmp,
+    #                                    align_corners=is_corner)
+    # add_str = "inv my data aug img from my data aug img which has resize "
+    # add_str += f"on {frame_name}"
+    # inv_img_tmp_crop = add_str_img(add_str, inv_img_tmp_crop[0])
     # out_img_list.append(orig_img[0])
     out_img_list.append(down_size_orig_img)
     out_img_list.append(img_str)
@@ -76,8 +98,9 @@ def debug_img_save(img_tensor, orig_img_pil, out_coord, out_grid, idx=1, calc_co
         out_img_list.append(img_calc_tmp)
         out_img_list.append(inv_img_calc_tmp)
     # out_img_list.append(down_size_orig_img)
-    # out_img_list.append(inv_img)
-    # out_img_list.append(inv_img_tmp)
+    out_img_list.append(inv_img)
+    out_img_list.append(inv_img_tmp)
+    out_img_list.append(inv_orig_img_tmp)
     # out_img_list.append(inv_img_tmp_crop)
     out_img = torch.stack(out_img_list)
 
